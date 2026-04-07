20f356e4353dcb2103bf8e9dcf05efc3071cb7ea
SongCodeMaster <lssyl_@naver.com>
2026-04-07 15:07:21 +0900

    docs: 거래 등록 페이지 컴포넌트 파일 등록

---
+++ src/views/TransactionRegister.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+<template>
+    <div class="RegisterTopView">
+        <div> 수입, 지출 버튼 컨테이너 </div>
+        <div> 금액 텍스트 필드 컨테이너 </div>
+    </div>
+
+    <div class="RegisterMidView"> 
+        <div> 날짜 선택 컨테이너 </div>
+        <div> 시간 선택 컨테이너 </div>
+        <div> 카테고리 컨테이너 </div>
+        <div> 메모 텍스트 필드 컨테이너 </div>
+        <div> 저장 버튼 컨테이너 </div>
+    </div>
+
+    <div class="RegisterBotView">
+        <div> 입력된 요약 컨테이너 </div>
+        <div> 최근 거래 리스트 컨테이너</div>
+    </div>
+
+</template>
