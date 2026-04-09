e37583411a977786f6684a2e15c5c9ad81c3ea87
SongCodeMaster <lssyl_@naver.com>
2026-04-09 09:50:10 +0900

    feat: 거래 등록, 수정, 삭제 API 파일 작성

---
+++ src/api/transactionsRegister.js
@@ -0,0 +1,17 @@
+import { P } from "vue-router/dist/index-BzEKChPW";
+import apiClient from "./index";
+
+// 거래 내역 추가 (POST)
+export const addTransaction  = (data) => {
+    return apiClient.post('/transactions', data);
+}
+
+// 거래 내역 수정 (PATCH)
+export const updateTransaction = (id, data) => {
+    return apiClient.patch(`transactions/${id}`, data);
+}
+
+// 거래 내역 삭제 (DELETE)
+export const deleteTransaction = (id) => {
+    return apiClient.delete(`transactions/${id}`, data)
+}
