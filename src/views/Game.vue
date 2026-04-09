4bddf124dd8665257a9c13b4fdf513c3eba5c035
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-04-09 11:10:45 +0900

    style: 높이 수정

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -15,36 +15,42 @@ const handleGameOver = () => {
 </script>
 
 <template>
-  <div class="game-page-container">
-    
-    <GameSetup 
-      v-if="currentPhase === 'READY'" 
-      @start-game="startGamePhase" 
-    />
+  <div class="top-container">
+    <div class="game-page-container">
+      <GameSetup 
+        v-if="currentPhase === 'READY'" 
+        @start-game="startGamePhase" 
+      />
 
-    <GameBoard 
-      v-else-if="currentPhase === 'PLAYING'" 
-      @game-over="handleGameOver" 
-    />
+      <GameBoard 
+        v-else-if="currentPhase === 'PLAYING'" 
+        @game-over="handleGameOver" 
+      />
 
-    <div v-else-if="currentPhase === 'END'" class="ending-screen">
-      <h1>팩트 폭격 준비 중... 🧾</h1>
+      <div v-else-if="currentPhase === 'END'" class="ending-screen">
+        <h1>팩트 폭격 준비 중... 🧾</h1>
+      </div>
     </div>
-
   </div>
 </template>
 
 <style scoped>
+.top-container {
+  margin: 0 !important;
+  padding: 0 !important;
+  overflow: hidden !important; 
+}
+
 .game-page-container {
   width: 100vw;
-  height: 100vh;
+  height: calc(100vh - 61px);
   margin: 0;
   padding: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #e9ecef;
-  overflow: hidden; /* 안전장치 */
+  overflow: hidden;
 }
 
 .ending-screen {
@@ -56,18 +62,5 @@ const handleGameOver = () => {
   font-size: 2rem;
   background-color: #fff;
   color: black;
-}
-</style>
-
-<style>
-html, body {
-  margin: 0 !important;
-  padding: 0 !important;
-  overflow: hidden !important; 
-}
-#app {
-  max-width: 100% !important;
-  margin: 0 !important;
-  padding: 0 !important;
 }
 </style>
